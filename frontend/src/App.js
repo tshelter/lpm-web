0c655d78706fcd52d72b0b2af66f8b65e192535e
haltinaa <ashimovaaltynai28@gmail.com>
2024-05-20 19:17:11 +0600

    footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import NavbarApp from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App d-flex flex-column min-vh-100">
       <header className="App-header">
         <NavbarApp />
       </header>
+      <main>
+
+      </main>
+      <div className="flex-grow-1"></div>
+      <Footer />
     </div>
   );
 }
