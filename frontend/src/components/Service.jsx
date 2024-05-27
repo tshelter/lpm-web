41ebf02e304aa2cc477b449e77bb11059d55ca0f
Łukasz Tshipenchko <dev@zxc.sx>
2024-05-27 22:25:42 +0500

    at least it works

--- frontend/src/components/Service.jsx
+++ frontend/src/components/Service.jsx
@@ -1,34 +1,70 @@
 import {v4 as uuid_v4} from "uuid";
-import React, {useState} from 'react';
-import {Button} from "react-bootstrap";
+import React, {useEffect, useRef, useState} from 'react';
+import {Button, Collapse} from "react-bootstrap";
+import {subscribe, unsubscribe} from "../events";
 
 
 function Service({ctx, agent_id, name, is_active, is_enabled, memory}) {
-    const callbacks = useState({});
-    const handleRestart = () => {
-        console.log("Restarting service", name);
+    const [isHandling, setIsHandling] = useState(false);
+    const [isShowLogs, setIsShowLogs] = useState(false);
+    const [logs, setLogs] = useState([]);
+    const waitingForUuid = useRef(null);
+
+    const handleAction = (action) => {
+        setIsHandling(true);
         const uuid = uuid_v4();
-        setIsRestarting(true);
-        ctx.ws.send(JSON.stringify({
+        waitingForUuid.current = uuid;
+        ctx.current.ws.send(JSON.stringify({
             agent_id,
             uuid,
             action: {
-                action: "restart",
+                action: action,
                 service: name,
             }
         }));
-        callbacks[uuid] = () => {
-            setIsRestarting(false);
-            callbacks[uuid] = null;
+    }
+
+    const createHandler = (action) => {
+        return () => {
+            handleAction(action);
         }
     }
-    document.addEventListener("lpm:action", (event) => {
-        if (event.uuid in callbacks) {
-            callbacks[event.uuid]();
-        }
-    });
 
-    const [isRestarting, setIsRestarting] = useState(false);
+    useEffect(() => {
+        const handleLogEvent = (event) => {
+            const data = event.detail;
+            if (data.agent_id !== agent_id || data.log.service !== name) {
+                return;
+            }
+            setLogs(prevLogs => [...prevLogs, data.log.message]);
+        }
+
+        subscribe("lpm:log", handleLogEvent);
+        return () => {
+            unsubscribe("lpm:log", handleLogEvent)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        const handleActionEvent = (event) => {
+            const {uuid, action} = event.detail;
+            console.log(uuid, action, waitingForUuid.current);
+            if (uuid !== waitingForUuid.current) {
+                return;
+            }
+            setIsHandling(false);
+            if (typeof action.response === "string" && action.response !== "ok") {
+                setLogs(prevLogs => [...prevLogs, `LPM Action Error: ${action.response}`]);
+            }
+        }
+
+        subscribe("lpm:action", handleActionEvent);
+        return () => {
+            unsubscribe("lpm:action", handleActionEvent)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <li>
@@ -37,7 +73,25 @@ function Service({ctx, agent_id, name, is_active, is_enabled, memory}) {
             <p>Active: {is_active ? "Yes" : "No"}</p>
             <p>Enabled: {is_enabled ? "Yes" : "No"}</p>
 
-            <Button onClick={handleRestart} disabled={isRestarting}>Restart</Button>
+            <Button onClick={createHandler("start")} disabled={isHandling}>Start</Button>
+            <Button onClick={createHandler("stop")} disabled={isHandling}>Stop</Button>
+            <Button onClick={createHandler("restart")} disabled={isHandling}>Restart</Button>
+            <Button onClick={createHandler("reload")} disabled={isHandling}>Reload</Button>
+            <Button onClick={createHandler("remove")} disabled={isHandling}>Remove</Button>
+
+            <Button onClick={() => setIsShowLogs(!isShowLogs)}>{isShowLogs ? "Hide" : "Show"} Logs</Button>
+            <Collapse in={isShowLogs}>
+                <div>
+                    <p>Logs:</p>
+                    <ul>
+                        {logs.map((log, index) => {
+                            return (
+                                <li key={index}>{log}</li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </Collapse>
         </li>
     );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,45 +1,43 @@
 import {v4 as uuid_v4} from "uuid";
 import config from "../config";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Agent from "../components/Agent";
+import {publish} from "../events";
 
 
 function HomePage() {
-    const client_id = uuid_v4().toString();
     const [agents, setAgents] = useState({});
+    const ctx = useRef({});
 
-    const ws = new WebSocket(`${config.WEBSOCKET_URL}/${client_id}`);
-    const ctx = {ws, client_id};
+    useEffect(() => {
+        const client_id = uuid_v4().toString();
+        const ws = new WebSocket(`${config.WEBSOCKET_URL}/${client_id}`);
+        ws.onmessage = (event) => {
+            const msg = JSON.parse(event.data);
 
-    const dispatchEvent = (type, payload) => {
-        document.dispatchEvent(new CustomEvent(
-            type, payload
-        ));
-    }
+            if (msg.status) {
+                const status = msg.status;
+                setAgents({...agents, [msg.agent_id]: <Agent key={msg.agent_id} agent_id={msg.agent_id} ctx={ctx} {...status}/>});
+            }
+            if (msg.action) {
+                publish("lpm:action", msg);
+            }
+            if (msg.log) {
+                publish("lpm:log", msg);
+            }
+        };
+        ctx.current.ws = ws;
 
-    ws.onmessage = (event) => {
-        const msg = JSON.parse(event.data);
-
-        if (msg.status) {
-            const status = msg.status;
-            setAgents({...agents, [msg.agent_id]: {services: status.services, agent_id: msg.agent_id}});
-            dispatchEvent("lpm:status", msg);
-        }
-        if (msg.action) {
-            dispatchEvent("lpm:action", msg);
-        }
-        if (msg.log) {
-            dispatchEvent("lpm:log", msg);
-        }
-    };
+        return () => {
+            ws.close();
+            ws.onmessage = null;
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <div>
-            {Object.values(agents).map((agent) => {
-                return (
-                    <Agent key={agent.agent_id} ctx={ctx} {...agent}/>
-                );
-            })}
+            {Object.values(agents)}
         </div>
     );
 }
